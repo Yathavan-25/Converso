574082b093347fa33ab0d969f553815c164ae15f
Yathavan <yathavanyathu27@gmail.com>
2025-09-16 16:58:08 +0530

    Implemented Authenticaiton, Billing, Database Setup, Create Companion Server action

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -19,11 +20,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
-      <NavBar />
-      {children}
-      </body>
-    </html>
+
+      <html lang="en">
+        <body className={`${bricolage.variable} antialiased`}>
+        <ClerkProvider appearance={{variables: {colorPrimary: '#fe5933'}}}>
+          <NavBar />
+          {children}
+        </ClerkProvider>
+        </body>
+      </html>
+
   );
 }
--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -36,7 +36,7 @@ const CompanionsList = ({ title, companions, classNames}:CompanionsListProps) =>
         </TableHeader>
         <TableBody>
           {companions.map(({ id, subject, name, topic, duration }) => (
-              <TableRow>
+              <TableRow key={id}>
                 <TableCell key={id}>
                   <Link href={`/companions/${id}`}>
                     <div className="flex items-center gap-2">
