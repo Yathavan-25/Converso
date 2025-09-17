1cb46d2c2ff9a1f0f9c0ab5e8be02c1d1ca841ad
Yathavan <yathavanyathu27@gmail.com>
2025-09-17 19:01:37 +0530

    implemented vapi companion

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    devIndicators:false
+    images : {
+        remotePatterns: [
+            {
+                hostname: 'img.clerk.com'
+            }
+        ]
+    }
 };
 
 export default nextConfig;
